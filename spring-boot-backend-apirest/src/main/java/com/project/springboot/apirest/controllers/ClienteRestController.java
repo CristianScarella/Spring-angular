8aad065cd3aec41fc26ecde0978cbebc3f69327a
Cristian Scarella <crisitianscarella09@hotmail.com>
2022-02-20 19:55:30 -0300

    Agreagado el responseStatus

--- spring-boot-backend-apirest/src/main/java/com/project/springboot/apirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/project/springboot/apirest/controllers/ClienteRestController.java
@@ -3,10 +3,12 @@ package com.project.springboot.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.springboot.apirest.models.entity.Cliente;
@@ -26,6 +28,7 @@ public class ClienteRestController {
 	}
 	
 	@GetMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.OK)
 	public Cliente show(@PathVariable Long id) {
 		return clienteService.findById(id);
 	}
