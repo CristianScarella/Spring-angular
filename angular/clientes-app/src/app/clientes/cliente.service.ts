66283598e9751ef6496cbd43d9c9cf01a29c169e
Cristian Scarella <crisitianscarella09@hotmail.com>
2022-02-20 19:01:27 -0300

    Implementado el metodo listar clientes en el front

--- angular/clientes-app/src/app/clientes/cliente.service.ts
+++ angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ClienteService {
+  private urlEndpoint :string = 'http://localhost:8080/api/clientes';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  getClientes(): Cliente[]{
-    return CLIENTES;
+  getClientes(): Observable<Cliente[]>{
+    //return CLIENTES;
+    return this.http.get<Cliente[]>(this.urlEndpoint);
   }
 }
