84c5628cb898faae895b561efc5e4b8ac8c5237b
Cristian Scarella <crisitianscarella09@hotmail.com>
2022-02-26 16:43:25 -0300

    Modifico y agrego la funcion de crearClientes

--- angular/clientes-app/src/app/clientes/cliente.service.ts
+++ angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ClienteService {
   private urlEndpoint :string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
     //return CLIENTES;
     return this.http.get<Cliente[]>(this.urlEndpoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers: this.httpHeaders})
   }
 }
